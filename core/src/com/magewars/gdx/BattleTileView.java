dff7c40fdffd6ddebb8754ed3389558991b48d19
cdf.easy <cdf.easy@gmail.com>
2018-05-16 23:06:46 +0300

    [*] test

--- core/src/com/magewars/gdx/BattleTileView.java
+++ core/src/com/magewars/gdx/BattleTileView.java
@@ -8,8 +8,10 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.Value;
+import com.badlogic.gdx.utils.Scaling;
 
 public class BattleTileView  extends Table {
     public BattleTileView(GdxGame gdxGame){
@@ -21,11 +23,13 @@ public class BattleTileView  extends Table {
         //  stageLayout.defaults().space(new Value.Fixed(1f));
         // stageLayout.row();
         row().expand();
-        add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("cobble_blood_10_new")));
-        add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("cobble_blood_3_new")));
+        Stack stack=new Stack(new Image(gdxGame.getFloorAtlas().getTiledDrawable("cobble_blood_10_new")),new Image(gdxGame.getFloorAtlas().getDrawable("sand_1"), Scaling.none));
+        //add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("cobble_blood_10_new")));
+       // add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("cobble_blood_3_new")));
+        add(stack);
         add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("crypt_10")));
         add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("marble_floor_1")));
         add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("swamp_1_new")));
-        add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("sand_1")));
+        add(new Image(gdxGame.getFloorAtlas().getTiledDrawable("cobble_blood_3_new")));
     }
 }
